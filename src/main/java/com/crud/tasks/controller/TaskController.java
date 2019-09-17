c6b752e638dabdfeaee0acf93115fe0de00338bf
Piotr Filarecki <pfilar2@wp.pl>
2019-09-17 17:06:51 +0200

    frontend v2

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/v1/task")
 public class TaskController {
